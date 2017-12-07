933bb19d391be8a3d06232dde6331b4b1747e0ef
Sophie Laturnus <SLaturnus@googlemail.com>
2017-12-07 15:24:15 +0100

    UNITTEST: Angle with null vector

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
 import numpy as np
+import sys
+
+sys.path.append('..')
 
 #### TEST GET_ANGLE #####
-from ..morphopy._utils import get_angle
+from morphopy._utils import get_angle
 
 
 def test_get_angle_with_orthogonal_vectors():
@@ -37,3 +40,12 @@ def test_get_angle_with_unnormalized_vector():
     r, d = get_angle(v0, v1)
     assert (r == 90 * np.pi / 180)
     assert (d == 90)
+
+
+def test_angle_btw_zero_and_v1():
+    v0 = np.array([0, 0, 0])
+    v1 = np.array([0, 1, 0])
+
+    r, d = get_angle(v0, v1)
+    assert (r == 0)
+    assert (d == 0)
