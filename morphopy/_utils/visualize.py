795f64104d8b3d076c2694fea8e332b5a4ce600f
ziweih <huang-ziwei@outlook.com>
2018-02-06 11:22:44 +0100

    clean-up.

--- morphopy/_utils/visualize.py
+++ morphopy/_utils/visualize.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_dendrites):
-    
+
     if view == 'xy':
         axis0 = 0
         axis1 = 1
@@ -12,17 +12,16 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
     elif view == 'yz':
         axis0 = 1
         axis1 = 2
-    
-    
+
     soma = df_paths[df_paths.type == 1].path[0][0]
     axon = df_paths[df_paths.type == 2]
     basal_dendrites = df_paths[df_paths.type == 3]
-    apical_dendrites = df_paths[df_paths.type == 4] 
+    apical_dendrites = df_paths[df_paths.type == 4]
 
     ax.scatter(0, 0, s=280, color='grey')
-    
+
     if plot_basal_dendrites and len(basal_dendrites)>0:
-        
+
         bdcolors_idx = np.linspace(0, 200, max(basal_dendrites.branch_order)+1).astype(int)
         bdcolors = np.vstack(plt.cm.Reds_r(bdcolors_idx))[:, :3]
 
@@ -31,13 +30,13 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
             path_id = row[0]
             path = row[1]['path'] - soma
             order = row[1]['branch_order']
-            bpt = path[0]     
+            bpt = path[0]
 
             dend_plot = ax.plot(path[:, axis0], path[:, axis1], color=bdcolors[int(order)])
             ax.scatter(bpt[axis0], bpt[axis1], color=bdcolors[int(order)], zorder=1)
 
     if plot_apical_dendrites and len(apical_dendrites)>0:
-        
+
         adcolors_idx = np.linspace(0, 200, max(apical_dendrites.branch_order)+1).astype(int)
         adcolors = np.vstack(plt.cm.Purples_r(adcolors_idx))[:, :3]
 
@@ -46,22 +45,22 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
             path_id = row[0]
             path = row[1]['path'] - soma
             order = row[1]['branch_order']
-            bpt = path[0]     
+            bpt = path[0]
 
             dend_plot = ax.plot(path[:, axis0], path[:, axis1], color=adcolors[int(order)])
             ax.scatter(bpt[axis0], bpt[axis1], color=adcolors[int(order)], zorder=1)
-    
+
     if plot_axon and len(axon)>0:
-        
+
         acolors_idx = np.linspace(0, 200, max(axon.branch_order)+1).astype(int)
         acolors = np.vstack(plt.cm.Blues_r(acolors_idx))[:, :3]
-        
+
         for row in axon.iterrows():
 
             path_id = row[0]
             path = row[1]['path'] - soma
             order = row[1]['branch_order']
-            bpt = path[0]     
+            bpt = path[0]
 
             axon_plot = ax.plot(path[:, axis0], path[:, axis1], color=acolors[int(order)])
             ax.scatter(bpt[axis0], bpt[axis1], color=acolors[int(order)], zorder=1)
@@ -73,9 +72,9 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
 
     ax.set_xlim(-lim, lim)
     ax.set_ylim(-lim, lim)
-    
+
     ax.set_title('{}'.format(view))
-    
+
     ax.axis('off')
 
     return ax
@@ -100,7 +99,7 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
 #         zlims = np.array([-10, max(zlims).astype(int) + 30])
 #     else:
 #         zlims = np.array([-max(abs(zlims)).astype(int) - 30, max(abs(zlims)).astype(int) + 30])
-        
+
 #     return xylims, zlims
 
 # def plot_skeleton(ax, df_paths, soma, axis0, axis1, lims):
@@ -117,7 +116,7 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
 
 #     colors = plt.cm.viridis.colors
 #     colors_idx = np.linspace(0, 255, max(df_paths.branch_order)+1).astype(int)
-        
+
 #     ax.scatter(soma[axis0], soma[axis1], s=280, color='grey')
 #     for row in df_paths.iterrows():
 
@@ -129,17 +128,17 @@ def plot_morph(ax, df_paths, view, plot_axon, plot_basal_dendrites, plot_apical_
 #         order = row[1]['branch_order']
 #         ax.plot(path[:, axis0], path[:, axis1], color=colors[colors_idx[int(order)]])
 #         ax.scatter(bpt[axis0], bpt[axis1], color=colors[colors_idx[int(order)]], zorder=1)
-    
+
 #     xylims, zlim = lims
 
 #     if axis0 == 2 and axis1 == 0: # ax2
 #         ax.set_xlim(zlim[0], zlim[1])
-#         ax.set_ylim(xylims[0], xylims[1])   
+#         ax.set_ylim(xylims[0], xylims[1])
 
 #     elif axis0 == 1 and axis1 == 2: # ax3
 #         ax.set_xlim(xylims[0], xylims[1])
 #         ax.set_ylim(zlim[0], zlim[1])
-     
+
 
 #     elif axis0 == 1 and axis1 == 0: # ax1
 #         ax.set_xlim(xylims[0], xylims[1])
