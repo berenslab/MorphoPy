ea6b172c683f9601780d60700e902fedf0fe4d44
ziweih <huang-ziwei@outlook.com>
2018-02-15 21:05:22 +0100

    finished preprocessing.py.

--- morphopy/_preprocessing.py
+++ morphopy/_preprocessing.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 import logging
 
+from ._morph import *
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-__all__ = ['Preprocessing']
+
+
+# __all__ = ['Preprocessing']
 
 def read_swc(filepath):
 
@@ -214,26 +218,23 @@ def sort_path_direction(df_paths):
     df_paths['connect_to_at'] = ''
     df_paths['connect_to_at'] = df_paths['connect_to_at'].apply(np.array)
 
-    for row in df_paths.iterrows():
+    path_ids_head = df_paths[df_paths.path.apply(lambda x: (x[0] == soma).all())].index
 
-        path_id = row[0]
-        path = row[1]['path']
+    if len(path_ids_head) > 0:
+        df_paths.loc[path_ids_head, 'connect_to'] = -1
+        df_paths.loc[path_ids_head, 'connect_to_at'] = pd.Series({path_id:soma for path_id in path_ids_head})
 
-        if (path[0] == soma).all():
-            df_paths.set_value(path_id, 'connect_to', -1)
-            df_paths.set_value(path_id, 'connect_to_at', soma)
-            continue
+    path_ids_tail = df_paths[df_paths.path.apply(lambda x: (x[-1] == soma).all())].index
+    if len(path_ids_tail) > 0:
+        df_paths.loc[path_ids_tail, 'path'] = df_paths.loc[path_ids_tail].path.apply(lambda x: x[::-1])
+        df_paths.loc[path_ids_tail, 'connect_to'] = -1
+        df_paths.loc[path_ids_tail, 'connect_to_at'] = pd.Series({path_id:soma for path_id in path_ids_tail})
 
-        if (path[-1] == soma).all():
-            df_paths.set_value(path_id, 'path', path[::-1])
-            df_paths.set_value(path_id, 'connect_to', -1)
-            df_paths.set_value(path_id, 'connect_to_at', soma)
-    
     new_target_paths = list(df_paths[~np.isnan(df_paths.connect_to)].index) # seed the first round of paths to check
 
     logging.info('  Checking path connection.')
     logging.info('\tTotal num of paths to check: {}'.format(len(df_paths)))    
-    logging.info('\tNumber of paths connected to soma: {}'.format(len(new_target_paths)))
+    logging.info('\tNumber of paths connected to soma: {}'.format(len(new_target_paths)-1))
     
     while np.count_nonzero(~np.isnan(df_paths.connect_to)) != len(df_paths):
         
@@ -242,34 +243,40 @@ def sort_path_direction(df_paths):
 
         target_paths = new_target_paths
         new_target_paths = [] # empty the list to hold new target paths for next round
+        logging.debug("  Current target paths {}".format(target_paths))
+        logging.debug("  Next target paths: {}".format(new_target_paths))
         
         for target_path_id in target_paths:
+            
+            if target_path_id == 0: continue
 
             target_path = df_paths.loc[target_path_id].path
-            
-            for row in df_paths.iterrows():
 
-                path_id = row[0]
-                path = row[1]['path']
+            path_ids_head = df_paths[df_paths.path.apply(lambda x: (x[0] == target_path[-1]).all())].index.tolist()
+            path_ids_head = [i for i in path_ids_head if i not in all_checked_paths]
+            logging.debug("\t{} paths connect to target path by their head: {}".format(len(path_ids_head), path_ids_head))
+            if len(path_ids_head) > 0:
+                df_paths.loc[path_ids_head, 'connect_to'] = target_path_id
+                df_paths.loc[path_ids_head, 'connect_to_at'] = pd.Series({path_id:target_path[-1] for path_id in path_ids_head})
+                new_target_paths = new_target_paths + path_ids_head
+                logging.debug("  \tadding {} to next target paths".format(path_ids_head))
+                
+            path_ids_tail = df_paths[df_paths.path.apply(lambda x: (x[-1] == target_path[-1]).all())].index.tolist()
+            path_ids_tail = [i for i in path_ids_tail if i not in all_checked_paths]
+            logging.debug("\t{} paths connect to target path by their tail: {}".format(len(path_ids_tail), path_ids_tail))
 
-                if path_id in all_checked_paths:
-                    continue
-                else:
-                    if (path[0] == target_path).all(1).any():
-
-                        df_paths.set_value(path_id, 'connect_to', target_path_id)
-                        df_paths.set_value(path_id, 'connect_to_at', target_path[np.where((path[0] == target_path).all(1))[0]])
-                        new_target_paths.append(path_id)
-                        continue
-                    
-                    if (path[-1] == target_path).all(1).any(): 
-                        df_paths.set_value(path_id, 'path', path[::-1])
-                        df_paths.set_value(path_id, 'connect_to', target_path_id)
-                        df_paths.set_value(path_id, 'connect_to_at', target_path[np.where((path[-1] == target_path).all(1))[0]])
-                        new_target_paths.append(path_id)
-             
+            if len(path_ids_tail) > 0:
+                df_paths.loc[path_ids_tail, 'path'] = df_paths.loc[path_ids_tail].path.apply(lambda x: x[::-1])
+                df_paths.loc[path_ids_tail, 'connect_to'] = target_path_id
+                df_paths.loc[path_ids_tail, 'connect_to_at'] = pd.Series({path_id:target_path[-1] for path_id in path_ids_tail})
+                new_target_paths = new_target_paths + path_ids_tail
+                logging.debug("  \tadding {} to next target paths".format(path_ids_tail))
+                
+        logging.debug('  Next target paths is: {}'.format(new_target_paths))
+                
         num_check_paths_after = len(list(df_paths[~np.isnan(df_paths.connect_to)].index))
         logging.info("\tNumber of paths checked: {}".format(num_check_paths_after))
+        
         if num_check_paths_before == num_check_paths_after:
             num_disconneted = len(df_paths) - num_check_paths_after
             logging.info('\tNumber of disconnected path(s): {}'.format(num_disconneted))
@@ -277,7 +284,186 @@ def sort_path_direction(df_paths):
     
     df_paths_drop = df_paths[np.isnan(df_paths.connect_to)] 
     df_paths = df_paths.drop(df_paths[np.isnan(df_paths.connect_to)].index)
-       
+
+    return df_paths, df_paths_drop
+
+def get_paths_nearest_to_tree(df_paths, df_drops, num_all_paths):
+    
+    """
+    Get paths in df_drops which stay nearest to the connected paths. 
+    
+    Paremeters
+    ----------
+    df_paths: 
+        DataFrame holding all connected paths
+    
+    df_drops:
+        DataFrame holding all disconnected paths
+        
+    Return 
+    ------
+    res: dict
+        {'p0': {'path': path,
+                'path_id': path_id_drop,
+                'radius': radius,
+                'type': t},
+         'target': {'path': path_tree,
+                     'path_id': path_id_tree,
+                     'radius': radius_tree,
+                     'type': type_tree
+                     } }
+
+    """
+    
+    res = []
+    for row in df_drops.iterrows():
+        
+        path_id_drop = row[0]
+        path = row[1].path
+        
+        path_id_tree = df_paths.path.apply(lambda x: np.sqrt(((x[-1]-path)**2).sum(1).min())).argmin()
+        distance_arr = np.sqrt(np.sum((path - df_paths.loc[path_id_tree].path[-1]) ** 2, 1))
+        
+        loc_nearest = distance_arr.argmin()
+        dist_nearest = distance_arr.min()        
+        
+        res.append((path_id_drop, path_id_tree, loc_nearest, dist_nearest))
+    
+    res = np.array(res)            
+    nearest_paths_data = res[np.where(res[:, 3] == res[:, 3].min())[0]]
+    
+    ### get all paths data
+    
+    num_paths_to_tree = len(nearest_paths_data)
+    
+    path_id_tree = nearest_paths_data[0][1]
+    path_tree = df_paths.loc[path_id_tree].path
+    radius_tree = df_paths.loc[path_id_tree].radius
+    type_tree = df_paths.loc[path_id_tree].type
+    
+    loc_nearest = int(nearest_paths_data[0][2])
+    distance_between = nearest_paths_data[0][3]
+    
+    res = {}
+
+    for i, datum in enumerate(nearest_paths_data):
+        
+        path_id_drop = datum[0]
+        path = df_drops.loc[path_id_drop].path
+        radius = df_drops.loc[path_id_drop].radius
+        
+        t = df_drops.loc[path_id_drop].type
+        
+        if loc_nearest == 0:
+            path = path 
+            point_connect = path[0]
+            radius_connect = radius[0]
+            
+            res['p{}'.format(i)] = {'path': path,
+                                    'path_id': path_id_drop,
+                                    'radius': radius,
+                                    'type': t}
+        elif loc_nearest == len(path)-1:
+            path = path[::-1]
+            point_connect = path[0]
+            radius_connect = radius[0]
+            
+            res['p{}'.format(i)] = {'path': path,
+                                    'path_id': path_id_drop,
+                                    'radius': radius,
+                                    'type': t}
+        else:
+            p0 = path[:loc_nearest+1][::-1] 
+            p1 = path[loc_nearest:]
+            
+            r0 = radius[:loc_nearest+1]
+            r1 = radius[loc_nearest:]
+            
+            point_connect = p0[0]
+            radius_connect = r0[0]
+            
+            if len(p0) > 1:
+                res['p0'.format(i)] = {'path': p0,
+                                        'path_id': path_id_drop,
+                                        'radius': r0,
+                                        'type': t}
+            if len(p1) > 1:
+                res['p1'.format(i)] = {'path': p1,
+                                    # 'path_id': len(df_paths) + len(df_drops),
+                                    'path_id': num_all_paths,
+                                    'radius': r1,
+                                    'type': t}
+    
+    if distance_between > 0:
+        logging.info("Path {} appended point {} at the end".format(path_id_tree, point_connect))
+        path_tree = np.vstack([path_tree, point_connect])
+        radius_tree = np.hstack([radius_tree, radius_connect])
+    
+    res['target'] = {'path': path_tree,
+                     'path_id': path_id_tree,
+                     'radius': radius_tree,
+                     'type': type_tree
+                     }    
+    
+    return res
+
+def reconnect_dropped_paths(df_paths, df_drops):
+    
+    df_paths = df_paths.copy()
+    df_drops = df_drops.copy()
+    num_dropped_paths = len(df_drops)
+    
+    while len(df_drops) > 0:
+        logging.info('Lenght of df_paths: {}; df_drops: {}'.format(len(df_paths), len(df_drops)))
+        num_all_paths = num_dropped_paths + len(df_paths)
+        paths_data = get_paths_nearest_to_tree(df_paths, df_drops, num_all_paths)
+
+        target = paths_data.pop('target')
+        path_id_tree = target['path_id']
+        path_tree = target['path']
+        radius_tree = target['radius']
+        tail_path_tree = path_tree[-1]
+        
+        df_paths.at[int(path_id_tree), 'path'] = path_tree
+        df_paths.at[int(path_id_tree), 'radius'] = radius_tree
+
+        if len(paths_data) == 1:
+
+            p = paths_data.pop('p0')
+
+            path_id_drop = p['path_id']
+            path_drop = p['path']
+            radius_drop = p['radius']
+            t = p['type']
+            logging.info('  Append {} to {}'.format(path_id_drop, path_id_tree))
+            df_paths.at[int(path_id_tree), 'path'] = np.vstack([path_tree[:-1], path_drop]) 
+            df_paths.at[int(path_id_tree), 'radius'] = np.hstack([radius_tree[:-1], radius_drop] ) 
+            df_drops.drop(path_id_drop, inplace=True)
+
+        else:
+            for key, values in paths_data.items():
+
+                p = paths_data[key]
+
+                path_id_drop = p['path_id']
+                path_drop = p['path']
+                logging.info('  Add row: {}, len: {}'.format(int(path_id_drop), len(path_drop)))
+                radius_drop = p['radius']
+                t = p['type']
+                df_paths.loc[int(path_id_drop)] = [t, path_drop, radius_drop, path_id_tree, tail_path_tree]            
+
+                try:
+                    logging.info('\tdrop {}'.format(int(path_id_drop)))
+                    df_drops.drop(path_id_drop, inplace=True)
+                except:
+                    pass
+
+            logging.info('Lenght of df_drops: {}'.format(len(df_drops)))
+    
+    return df_paths.sort_index()
+
+def find_connection(df_paths):
+    
     # find all paths connect to current path.
     connected_by_dict = {}
     connected_by_at_dict = {}
@@ -298,8 +484,8 @@ def sort_path_direction(df_paths):
     df_paths['back_to_soma'] = pd.Series(back_to_soma_dict)
     
     logging.info('  Done.\n')
-
-    return df_paths, df_paths_drop
+    
+    return df_paths
 
 def write_swc(df_paths):
 
@@ -338,10 +524,10 @@ def write_swc(df_paths):
 
             else:
                 pid = np.where((swc_arr[:, 2:5] == connect_to_at).all(1))[0] + 1
-#                 print(pid)
+#                 logging.info(pid)
                 if len(pid)>1:
                     swc_path[0][-1] = pid[0]
-#                     print(swc_arr[pid[0]][2:5], swc_arr[pid[1]][2:5])
+#                     logging.info(swc_arr[pid[0]][2:5], swc_arr[pid[1]][2:5])
                 else:
                     swc_path[0][-1] = pid
                 swc_arr = np.vstack([swc_arr, swc_path])
@@ -355,7 +541,7 @@ def write_swc(df_paths):
     
     return df_swc
 
-class Preprocessing:
+class Preprocessing(Morph):
 
     def __init__(self, filepath):
 
@@ -380,11 +566,15 @@ class Preprocessing:
         edge_dict = get_edge_dict(n, e, soma_loc)
         df_paths = get_path_dict(pos, radius, t, edge_dict, soma_loc)
         df_paths, df_paths_drop = sort_path_direction(df_paths)
+        df_paths = reconnect_dropped_paths(df_paths, df_paths_drop)
+        df_paths = find_connection(df_paths)
         df_swc = write_swc(df_paths)
         
         self.df_paths = df_paths
+        self.df_paths_drop = df_paths_drop
         self.df_swc = df_swc
+        self.filename = filename
 
-    def save_as_swc(self, filename, save_to='./'):
+    def save_as_swc(self, save_to='./'):
 
-        self.df_swc.to_csv(save_to + '{}.swc'.format(filename), sep=' ', index=None, header=None)
+        self.df_swc.to_csv(save_to + '{}.swc'.format(self.filename), sep=' ', index=None, header=None)
